cfc3acf53b2e15c7eaa98370ace36ecb1025f570
renjiajian <renjiajian@withub.net.cn>
2017-07-31 19:35:23 +0800

    。。。

--- app/src/main/java/com/unicorn/sxshenwutong/e/jasc/JascRenderer.java
+++ app/src/main/java/com/unicorn/sxshenwutong/e/jasc/JascRenderer.java
@@ -1,9 +1,8 @@
 package com.unicorn.sxshenwutong.e.jasc;
 
 import com.chad.library.adapter.base.BaseViewHolder;
-import com.unicorn.sxshenwutong.R;
-import com.unicorn.sxshenwutong.e.base.AjxxRender;
 import com.unicorn.sxshenwutong.d.spdb.Ajxx;
+import com.unicorn.sxshenwutong.e.base.AjxxRender;
 
 class JascRenderer extends AjxxRender {
 
@@ -11,11 +10,15 @@ class JascRenderer extends AjxxRender {
         super(baseViewHolder, ajxx);
     }
 
+    /*
+    案号、案由、立案日期、原告、被告、结案日期、结案方式
+     */
+
     @Override
     public void render() {
         super.render();
-        viewHolder.setText(R.id.tvJarq, "结案日期 :" + entity.getJarq());
-        viewHolder.setText(R.id.tvJafsmc, "结案方式 :" + entity.getJafsmc());
+//        viewHolder.setText(R.id.tvJarq, "结案日期 :" + entity.getJarq());
+//        viewHolder.setText(R.id.tvJafsmc, "结案方式 :" + entity.getJafsmc());
     }
 
 }
